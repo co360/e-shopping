fc537351453c04b12ba782b099b3a84e3d53b7e1
Thaneshwar Sahu <thaneshwar.sahu@globallogic.com>
2019-08-27 13:12:03 +0530

    add order

---
+++ src/main/java/com/gl/eshopping/model/OrderProduct.java
@@ -0,0 +1,30 @@
+package com.gl.eshopping.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "order_product")
+public class OrderProduct extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+//    @JsonBackReference(value = "product-orderProducts")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    @JsonBackReference(value = "order-orderProducts")
+    private Order order;
+
+
+    private Integer quantity;
+    private Double totalPrice;
+}
